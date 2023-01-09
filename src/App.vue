21950add0675fdbcbc7e89cb4ccb3e621ab4dfcc
HeppeDavardo <Jeppedavid1@gmail.com>
2023-01-09 22:27:11 +0100

    styling

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,11 @@
       @click="currentTab = tab">
       {{ tab }}
     </button>
+    <section class="menuImg">
+      <img src="../public/assets/img/sandwich.jpg" alt="">
+    </section>
   </section>
-    <component :is="currentTab" class="tab"></component>
+    <component :is="currentTab" class="tab menuTxt"></component>
   </section>
 </template>
 
@@ -41,35 +44,125 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 @import url("https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap");
-#app {
-  font-family: "Titillium Web", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.centerMenu {
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 .vueMenu {
+  font-family: "Titillium Web", sans-serif;
   display: flex;
+  margin-top: 5rem;
+  padding-right: 20rem;
+  min-height: 100vh;
 }
 .menuNav {
+  position: relative;
   display: flex;
   flex-direction: column;
-  gap: 1rem;
-  width: 20%;
-  margin-top: 5rem;
-  padding-left: 8rem;
+  gap: 1.2rem;
+  padding-top: 1rem;
+  padding-left: 20rem;
   border-right: 2px solid #707070;
 }
 .menuNavBtn{
+  font-family: "Titillium Web", sans-serif;
+  color: #646B41;
   background-color: transparent;
   border: none;
   text-align: left;
-  font-size: 20px;
+  font-size: 1.9rem;
   font-weight: normal; 
   cursor: pointer;
+  width: 250px;
+}
+.menuNavBtn.active, 
+.menuNavBtn:hover {
+  border-right: 4px solid #646B41;
+
+}
+
+.menuImg {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  top: 20rem;
+  left: 0;
+  width: 100%;
+  height: 80%;
+  z-index: -10;
+  pointer-events: none;
+  object-fit: cover;
+  overflow: hidden;
+  
+}
+
+
+.menuTxt {
+  padding-left: 3rem;
+  color: #4D594D;
+}
+.dish {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  align-items: end;
+}
+
+#menupunkt {
+  color: #646B41;
+  font-size: 3.5rem;
+  font-weight: 600;
+  margin: 0;
+}
+.menupunktDesc {
+  margin: 2rem 0 4rem;
+  font-size: 1.5rem;
+  color: #646B41;
+}
+#kategori{
+  color: #8AA08A;
+  margin: 0;
+  padding: 3rem 0 0.5rem;
+  font-size: 1.8rem;
+}
+
+#navn {
+  margin: 0;
+  padding-bottom: 1rem;
+  font-weight: 600;
+  font-size: 1.5rem;
+}
+#ingredienser {
+  margin: 0;
+  padding-bottom: 1rem;
+  font-size: 1.5rem;
+}
+#pris {
+  margin: 0;
+  padding-bottom: 1rem;
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+/* Hvis at kontaineren er tom, skal den ikke vises */
+.dish:empty {
+  display: none;
+}
+#navn:empty {
+  display: none;
+}
+#ingredienser:empty {
+  display: none;
+}
+#pris:empty {
+  display: none;
 }
 </style>
--- src/components/IndenMaden.vue
+++ src/components/IndenMaden.vue
@@ -39,34 +39,50 @@ export default {
                 console.log(data);
                 let ret = document.querySelector('#ret');
 
-
+                ret.innerHTML = `<h2 id="menupunkt">Inden maden</h2>`;
                 for (let i = data.length-1; i >= 0; i--) {
                     ret.innerHTML += `
-                    <h1>${data[i].acf.kategori}</h1>
-                    <h2>${data[i].acf.navn1}</h2>
-                    <p>${data[i].acf.ingredienser1}</p>
-                    <h2>${data[i].acf.pris1}</h2>
-                    <h2>${data[i].acf.navn2}</h2>
-                    <p>${data[i].acf.ingredienser2}</p>
-                    <h2>${data[i].acf.pris2}</h2>
-                    <h2>${data[i].acf.navn3}</h2>
-                    <p>${data[i].acf.ingredienser3}</p>
-                    <h2>${data[i].acf.pris3}</h2>
-                    <h2>${data[i].acf.navn4}</h2>
-                    <p>${data[i].acf.ingredienser4}</p>
-                    <h2>${data[i].acf.pris4}</h2>
-                    <h2>${data[i].acf.navn5}</h2>
-                    <p>${data[i].acf.ingredienser5}</p>
-                    <h2>${data[i].acf.pris5}</h2>
-                    <h2>${data[i].acf.navn6}</h2>
-                    <p>${data[i].acf.ingredienser6}</p>
-                    <h2>${data[i].acf.pris6}</h2>
-                    <h2>${data[i].acf.navn7}</h2>
-                    <p>${data[i].acf.ingredienser7}</p>
-                    <h2>${data[i].acf.pris7}</h2>
-                    <h2>${data[i].acf.navn8}</h2>
-                    <p>${data[i].acf.ingredienser8}</p>
-                    <h2>${data[i].acf.pris8}</h2>
+                    <h2 id="kategori">${data[i].acf.kategori}</h2>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn1}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser1}</p>
+                        <h3 id="pris">${data[i].acf.pris1}</h3>
+                    </article>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn2}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser2}</p>
+                        <h3 id="pris">${data[i].acf.pris2}</h3>
+                    </article>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn3}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser3}</p>
+                        <h3 id="pris">${data[i].acf.pris3}</h3>
+                    </article>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn4}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser4}</p>
+                        <h3 id="pris">${data[i].acf.pris4}</h3>
+                    </article>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn5}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser5}</p>
+                        <h3 id="pris">${data[i].acf.pris5}</h3>
+                    </article>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn6}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser6}</p>
+                        <h3 id="pris">${data[i].acf.pris6}</h3>
+                    </article>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn7}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser7}</p>
+                        <h3 id="pris">${data[i].acf.pris7}</h3>
+                    </article>
+                    <article class="dish">
+                        <h3 id="navn">${data[i].acf.navn8}</h3>
+                        <p id="ingredienser">${data[i].acf.ingredienser8}</p>
+                        <h3 id="pris">${data[i].acf.pris8}</h3>
+                    </article>
                     `;
                 }
             });
